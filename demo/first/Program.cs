2c45fcfc9edf003fc03ae668a7e9e79d2928af29
Pankaj Walke <120715164+pankajwalke@users.noreply.github.com>
2023-06-24 22:59:17 +0530

    Update Program.cs

--- demo/first/Program.cs
+++ demo/first/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("pankaj walke");
-Console.WriteLine("ashok walke");
-Console.WriteLine("yamutai walke");
+
