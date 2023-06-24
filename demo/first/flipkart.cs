4ac60ccf87b08cf268e5a703b3e5978ec8c1d48d
Mahesh Shende <pankajwalke00@gmail.com>
2023-06-25 00:33:00 +0530

    newly class added

---
+++ demo/first/flipkart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    internal class amazon
+    {
+        public int a;
+        public int b;
+
+        public  int main()
+        {
+            return a + b;
+        }
+    }
+}
